20efa8258b43e722cc1c6da599061f9b1d1dcbdc
Gease-RS <geasebrasil@gmail.com>
2020-02-13 18:22:30 -0300

    Criando listagem

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const multer = require('multer');
-const uploadConfing = require('./config/upload');
+const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
-const upload = multer(uploadConfing);
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
